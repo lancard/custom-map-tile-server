59657254cd7380b81ce52eb0eb22aabadb2c3b4b
Sung-ho Kim <42758339+lancard@users.noreply.github.com>
2022-11-26 14:39:39 +0900

    Update prebuild.sh

--- prebuild.sh
+++ prebuild.sh
@@ -2,7 +2,7 @@
 
 docker run \
 	--rm \
-	-v osm-data:/data/database/ \
+	-v osm-data-backup:/data/database/ \
 	-e DOWNLOAD_PBF=https://github.com/lancard/filtered-south-korea-osm/raw/main/filtered.osm.pbf \
 	-e DOWNLOAD_POLY=https://download.geofabrik.de/asia/south-korea.poly \
 	overv/openstreetmap-tile-server \
