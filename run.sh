e6d1ef34676668efe6ab2fc28d30cbff91987ec7
Sungho Kim <worstrocker@gmail.com>
2022-12-04 18:45:23 +0900

    change docker image

--- run.sh
+++ run.sh
@@ -6,11 +6,11 @@ rm -rf tiles_default
 mkdir tiles_default
 chmod 777 tiles_default
 rm -f style/mapnik.xml
+
 docker run \
 	-p 8080:80 -p 5432:5432 \
-	-v osm-data:/data/database/ \
 	-v $PWD/tiles_default:/data/tiles/default \
 	-v $PWD/style:/data/style \
 	-e NAME_MML=fans.mml \
-	-d overv/openstreetmap-tile-server \
+	-d lancard/tile-server \
 	run
