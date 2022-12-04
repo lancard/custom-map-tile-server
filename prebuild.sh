fa066fcda28b2a8ea789be9ae86ad600ac716792
Sungho Kim <worstrocker@gmail.com>
2022-12-04 18:42:11 +0900

    delete unnecessary resources

--- prebuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run \
-	--rm \
-	-v osm-data-backup:/data/database/ \
-	-e DOWNLOAD_PBF=https://github.com/lancard/filtered-south-korea-osm/raw/main/filtered.osm.pbf \
-	-e DOWNLOAD_POLY=https://download.geofabrik.de/asia/south-korea.poly \
-	overv/openstreetmap-tile-server \
-	import
