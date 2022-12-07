e83259ab9e165c5d72e86779605b19d50acfc54f
Sungho Kim <worstrocker@gmail.com>
2022-12-07 14:42:09 +0900

    modify bug

--- debug.sh
+++ debug.sh
@@ -7,4 +7,9 @@ mkdir tiles_default
 chmod 777 tiles_default
 rm -f style/mapnik.xml
 
-docker run -it --entrypoint /bin/bash lancard/tile-server
+docker run -it --entrypoint /bin/bash \
+        -p 8080:80 -p 5432:5432 \
+        -v $PWD/tiles_default:/data/tiles/default \
+        -v $PWD/style:/data/style \
+        -e NAME_MML=fans.mml \
+	lancard/tile-server
---
+++ style/build.sh
@@ -0,0 +1,2 @@
+carto fans.mml > mapnik.xml
+sudo -u renderer renderd -f -c /etc/renderd.conf
