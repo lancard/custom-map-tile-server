2223cc430360efce19e320d6447b5b730abad74f
Sungho Kim <worstrocker@gmail.com>
2022-12-08 17:35:36 +0900

    update build tiles.sh

--- build_tiles.sh
+++ build_tiles.sh
@@ -4,3 +4,8 @@ rm -rf 0 1 2 3 4 5 6 7 8 9 10 11 12 13
 cd ..
 npm i graceful-fs
 node index.js
+cd korea-openstreetmap-tiles
+git add .
+git commit -m "update tiles"
+git push origin HEAD:main
+cd ..
